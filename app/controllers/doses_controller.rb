3509fdad5b30c67218ce5b560a5ab584cecaa246
Jake Engel <jakeengel01@gmail.com>
2017-11-09 17:13:20 +0100

    Finished all the views for the rails-mister-cocktail challenge! Wow this is great.

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,23 +1,18 @@
 class DosesController < ApplicationController
   before_action :find_cocktail, only: [ :new , :create ]
 
-  def index
-    @doses = Dose.all
-  end
-
-  def show
-    @cocktail = Cocktail.find(params[:id])
-  end
-
   def new
     @dose = Dose.new
+    @ingredients = Ingredient.all
   end
 
   def create
-    @dose = @cocktail.doses.new(dose_params)
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
     if @dose.save
       redirect_to cocktail_path(@cocktail)
     else
+      @ingredients = Ingredient.all
       render :new
     end
   end
@@ -25,6 +20,8 @@ class DosesController < ApplicationController
   def destroy
     @dose = Dose.find(params[:id])
     @dose.destroy
+
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
